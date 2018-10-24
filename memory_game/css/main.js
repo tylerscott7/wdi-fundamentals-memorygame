507bac4a5c738a82242bc298ef49c360e9a6694f
Tyler Nicholson <tsnicholson7@gmail.com>
2018-10-23 23:18:20 -0600

    created game logic

--- memory_game/css/main.js
+++ memory_game/css/main.js
@@ -11,7 +11,7 @@ console.log("User flipped " + cardTwo);
 if (cardsInPlay.length === 2){
 	if (cardsInPlay[0]=== cardsInPlay[1]){
 		alert("You found a match!");
-	} else {
+	} else{
 		alert("Sorry, try again.");
-	};
+	}
 };
