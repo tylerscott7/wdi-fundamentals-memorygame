16158351ac0f709bab77a44a480d77cccff12071
Tyler Nicholson <tsnicholson7@gmail.com>
2018-10-25 22:15:47 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank:'queen',
+suit:'hearts',
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank:'queen',
+suit:'diamonds',
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank:'king',
+suit:'hearts',
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank:'king',
+suit:'diamonds',
+cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -11,7 +33,9 @@ var checkForMatch = function(){
 
 var flipCard = function (cardID){
 	checkForMatch();
-	console.log("User flipped " + cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log("User flipped " + cards[cardID].cardImage);
+	console.log("User flipped " + cards[cardID].suit);
 	cardsInPlay.push(cards[cardID]);
 };
 
